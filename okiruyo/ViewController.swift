0705d9241c170d310430279ab2f830ba6a864319
MasatakaKudou <b1017027@fun.ac.jp>
2020-01-20 20:17:01 +0900

    [add]new file

---
+++ okiruyo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  okiruyo
+//
+//  Created by 工藤正隆 on 2020/01/20.
+//  Copyright © 2020 工藤正隆. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
