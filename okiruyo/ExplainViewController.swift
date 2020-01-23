6dfee905782394f6c2f12d90274c8faf3a2dc431
MasatakaKudou <b1017027@fun.ac.jp>
2020-01-24 08:10:07 +0900

    [add]appIconの追加,display name変更

---
+++ okiruyo/ExplainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ExplainViewController.swift
+//  okiruyo
+//
+//  Created by 工藤正隆 on 2020/01/24.
+//  Copyright © 2020 工藤正隆. All rights reserved.
+//
+
+import UIKit
+
+class ExplainViewController: UIViewController {
+    
+    @IBOutlet weak var backButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.backButton.layer.cornerRadius = 10.0
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
