294a7e7dc12231cd1531aa553e14aea033250f68
MasatakaKudou <b1017027@fun.ac.jp>
2020-01-24 06:58:15 +0900

    [add]操作説明の遷移を増やしや

--- okiruyo/ViewController.swift
+++ okiruyo/ViewController.swift
@@ -11,13 +11,16 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var beginButton: UIButton!
- 
+    @IBOutlet weak var explainButton: UIButton!
+    @IBOutlet weak var iconImage: UIImageView!
+    var okiruyoIcon = UIImage(named: "okiruyo_icon")!
     
     // アプリの画面上に表示されたときに自動的に呼び出される
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        iconImage.image = okiruyoIcon
         self.beginButton.layer.cornerRadius = 10.0
+        self.explainButton.layer.cornerRadius = 10.0
     }
 
 
